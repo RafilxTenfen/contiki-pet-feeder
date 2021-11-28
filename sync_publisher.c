8aa958ab3e965502d4dbf627382be8747646de74
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2021-11-28 10:23:12 -0300

    test makefile

--- sync_publisher.c
+++ sync_publisher.c
@@ -39,7 +39,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <curl/curl.h>
+// #include <curl/curl.h>
+
+#include "/usr/include/curl/curl.h"
 #include <time.h>
 
 typedef struct Config {
